08caff6fe1f925187d8c3597d92c6aae8c62925a
Jessie Boudreau <jessie.boudreau@live.ca>
2019-11-20 22:41:21 -0500

    New build

---
+++ build/precache-manifest.09a88306e9588cef28a522f6be437cbf.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c3e007f0fdc18845d32b514bfb078b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa54bd40338a914d7ffa",
+    "url": "/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "825d635390142b8bcde4",
+    "url": "/static/css/main.89f22aa0.chunk.css"
+  },
+  {
+    "revision": "aa54bd40338a914d7ffa",
+    "url": "/static/js/2.54f2dade.chunk.js"
+  },
+  {
+    "revision": "825d635390142b8bcde4",
+    "url": "/static/js/main.5c3d98f8.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  },
+  {
+    "revision": "3e7c6fe7b85d1462de65ff7a6a7f51f2",
+    "url": "/static/media/batterydesign-thumbnail.3e7c6fe7.png"
+  },
+  {
+    "revision": "0b24a3a0b18c1d0b8c2ce7bf5664a15b",
+    "url": "/static/media/cellboardtester-thumbnail.0b24a3a0.png"
+  },
+  {
+    "revision": "9be3091111a51418ddbc3868b0c3ea30",
+    "url": "/static/media/cells.9be30911.png"
+  },
+  {
+    "revision": "0c6e2cdad9f5321369a5e0cd64abeff6",
+    "url": "/static/media/exploded-container-view.0c6e2cda.png"
+  },
+  {
+    "revision": "ea452212ea0748961124d3a7cd13427e",
+    "url": "/static/media/jessie-boudreau-resume.ea452212.pdf"
+  },
+  {
+    "revision": "3e7c6fe7b85d1462de65ff7a6a7f51f2",
+    "url": "/static/media/original.3e7c6fe7.png"
+  },
+  {
+    "revision": "ae1f90c178b86ec21cdfc7278ea39411",
+    "url": "/static/media/original.ae1f90c1.png"
+  },
+  {
+    "revision": "054499d2d50bae19bbd419c533e1f6e3",
+    "url": "/static/media/packaging.054499d2.png"
+  },
+  {
+    "revision": "78a0dbd017a91875d7c727d89e2c72ff",
+    "url": "/static/media/thermal-model.78a0dbd0.png"
+  },
+  {
+    "revision": "571e1419691adaf6cfcc4ed7fd13ff94",
+    "url": "/static/media/watergenerator-thumbnail.571e1419.jpg"
+  }
+]);
